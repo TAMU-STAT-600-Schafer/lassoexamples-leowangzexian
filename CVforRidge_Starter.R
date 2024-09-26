cfa73d115aad5d5caec5c6a729a7663d82913479
Leo Zexian Wang <leowang@tamu.edu>
2024-09-26 10:54:30 -0500

    updated

--- CVforRidge_Starter.R
+++ CVforRidge_Starter.R
@@ -18,7 +18,8 @@ Y = beta0 + X %*% beta + rnorm(n, sd = sigma)
 # Split the data into K = 2 folds
 # [ToDo] Create idfold vector of length n indicating the fold id, should have 1 or 2 in each position
 K = 2
-idfolds = sample(K, sample = n, replace = TRUE) # non-equal fold sizes, head(idfolds), table(idfolds)
+# idfolds = sample(K, size = n, replace = TRUE) # non-equal fold sizes, head(idfolds), table(idfolds)
+idfolds = sample(rep(1:K, length.out=n), size=n) # no replacement
 
 # For loop over folds and lambdas
 ##########################################
@@ -27,29 +28,36 @@ nlambda = length(lambda_seq)
 cvm = rep(NA, nlambda) # want to have CV(lambda)
 cvse = rep(NA, nlambda) # want to have SE_CV(lambda)
 # Feel free to store additional things
-K = 7
+# K = 7
 cv_folds = matrix(NA, K, nlambda) # fold-specific errors
 
 
 idfold= sample(rep(1:K, length.out = n), size = n)
 for (fold in 1:K){
   #[ToDo] Create training data xtrain and ytrain, everything except fold
-
+  xtrain = X[idfolds != fold, ]
+  ytrain = Y[idfolds != fold]
   #[ToDo] Create testing data xtest and ytest, everything in fold
-
+  xtest = X[idfolds == fold, ]
+  ytest = Y[idfolds == fold]
   
   #[ToDo] Center training data 
-
-
+  meanY = mean(ytrain)
+  ytrain_centered = ytrain - meanY
+  
+  xtrain_centered = scale(xtrain, scale = FALSE) # attributes(), ?scale
+  meanX = colMeans(xtrain)
+  ntrain = nrow(xtrain)
   #[ToDo] For loop over lambdas
   for (i in 1:length(lambda_seq)){
     # [ToDo] Calculate ridge solution
-
-    # Get back the intercept: beta0 = barY - bar X^{top}beta
+    beta = solve(t(xtrain_centered)%*%xtrain_centered/ntrain + lambda_seq[i]*diag(p), t(xtrain_centered)%*%ytrain_centered/ntrain)
     
+    # Get back the intercept: beta0 = barY - bar X^{top}beta
+    beta0 = as.numeric(meanY - crossprod(meanX, beta))
     # [ToDo] Complete with anything else you need for cvm and cvse
-
-      }
+    cv_folds[fold, i] = mean((ytest - beta0 - xtest %*% beta)^2)
+    }
 }
 
 # [ToDo] get cvm and cvse from cv_folds
